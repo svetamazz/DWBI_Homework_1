7c81ecc459c88dbea6fe1de8203c1753d99516c6
Svitlana Matskiv <swetamazkiw@gmail.com>
2019-05-29 16:07:21 +0300

    8 file is done

--- 8_synonyms_creation.sql
+++ 8_synonyms_creation.sql
@@ -1,3 +1,32 @@
 -- 8 synonyms creation --
 USE [s_matskiv_module_2];
 GO
+
+-- schema for synonyms creation --
+CREATE SCHEMA syn;
+GO
+
+-- sysnonym to parent_author table
+CREATE SYNONYM syn.author FOR dbo.parent_author
+GO
+-- retrieve data from synonym to parent_author
+SELECT * FROM syn.author;
+GO
+SELECT age
+FROM syn.author
+WHERE first_name='Max';
+GO
+
+-- sysnonym to person_second table
+CREATE SYNONYM syn.person_second FOR dbo.person_second
+GO
+-- retrieve data from sysnonym to get_books_of_1_edition view
+SELECT *
+FROM syn.person_second
+
+-- sysnonym to get_books_of_1_edition view
+CREATE SYNONYM syn.books_view FOR dbo.get_books_of_1_edition
+GO
+-- retrieve data from sysnonym to get_books_of_1_edition view
+SELECT *
+FROM syn.books_view
